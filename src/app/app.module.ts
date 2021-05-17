f646208d1f3c77c6196714c5e25d61c8f05f8085
Maciej Kędziora <maciejkedziora98@gmail.com>
2021-05-17 11:39:37 +0200

    delete components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { WeekComponent } from './week/week.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-	declarations: [AppComponent, SidenavComponent, WeekComponent, ToolbarComponent],
+	declarations: [
+		AppComponent,
+	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
-		MatSidenavModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
--- src/app/week/week.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-week',
-	templateUrl: './week.component.html',
-	styleUrls: ['./week.component.scss'],
-})
-export class WeekComponent {}
