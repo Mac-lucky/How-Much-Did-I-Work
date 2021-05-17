a7fdefc5abc0f9f74f9846f75a8facd6bbfbcd51
Maciej Kędziora <maciejkedziora98@gmail.com>
2021-05-17 11:53:06 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WeekComponent } from './week/week.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
 	{ path: '', component: AppComponent },
-	{ path: 'week', component: WeekComponent },
 ];
 
 @NgModule({
